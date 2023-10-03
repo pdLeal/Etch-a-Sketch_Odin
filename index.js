54a4b6ab8e0c00393af2e440ddd5bd488ed593d6
pdLeal <lealpd@outlook.com>
2023-10-03 17:16:52 -0300

    feat: changeColor function n .newColor style created   also add eventListener inside the for loop

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 const container = document.getElementById("container");
 for (let i = 1; i <= 256; i++) {
     const div = document.createElement("div");
-    div.classList.add("grid", "item" + i);
+    div.classList.add("grid-item");
     container.appendChild(div);
+    div.addEventListener("mouseover", changeColor)
+}
+
+function changeColor(evt) {
+    evt.target.classList.add("newColor");
 }
