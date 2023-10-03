bd9ab129cdc443d097512e7cee76f216683d015d
pdLeal <lealpd@outlook.com>
2023-10-03 20:51:21 -0300

    feat: newGrid function and a button to listen for it

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// Creates 16x16 div-grid
 const container = document.getElementById("container");
-for (let i = 1; i <= 256; i++) {
+for (let i = 1; i <= 256; i++) { // Creates 16x16 div-grid
     const div = document.createElement("div");
     div.classList.add("grid-item");
+    div.style.width = `${100 / 16}%`
+    div.style.height = `${100 / 16}%`
     container.appendChild(div);
     div.addEventListener("mouseover", changeColor)
 }
@@ -10,3 +11,27 @@ for (let i = 1; i <= 256; i++) {
 function changeColor(evt) {
     evt.target.classList.add("newColor");
 }
+
+function newGrid() {
+    let promp = Number(prompt("How many squares by side?\n(Max of 100)"));
+
+    if(promp > 100 || isNaN(promp)) {
+        alert("Such a rebel, now choose up to 100 please")
+        return newGrid();
+    }
+
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    for (let i = 1; i <= (promp * promp); i++) {
+        const div = document.createElement("div");
+        div.classList.add("grid-item");
+        div.style.width = `${100 / promp}%`
+        div.style.height = `${100 / promp}%`
+        container.appendChild(div);
+        div.addEventListener("mouseover", changeColor)
+    }
+}
+const btnNewGrid = document.getElementById("btn-new-grid");
+btnNewGrid.addEventListener("click", newGrid);
